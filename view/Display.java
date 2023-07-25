6e3e800f5c9d2ea2d7b3e8a0b24d5291999633f5
Alan To <alantofus@gmail.com>
2023-07-25 13:12:10 -0700

    Updated GUI and fixed icon problems.

--- view/Display.java
+++ view/Display.java
@@ -42,7 +42,7 @@ public class Display extends JPanel {
         super.paintComponent(g);
         Room [][] mazeMap = myMaze.getMaze();
         Graphics2D g2d = (Graphics2D) g;
-        playerIcon = new ImageIcon("src/images/Comic_Books.png");
+        playerIcon = new ImageIcon("images/Comic_Books.png");
         //draw border for playing field
         g2d.setColor(Color.YELLOW); //sets the border/line to yellow
 
@@ -111,7 +111,7 @@ public class Display extends JPanel {
     }
 
     public void createNorthDoor() {
-        doorIcon = new ImageIcon("src/images/door.png");
+        doorIcon = new ImageIcon("images/door.png");
 
         //scaling image down.
         Image image = doorIcon.getImage();
@@ -132,7 +132,7 @@ public class Display extends JPanel {
     }
 
     public void createWestDoor() {
-        doorIcon = new ImageIcon("src/images/door.png");
+        doorIcon = new ImageIcon("images/door.png");
         //scaling image down.
         Image image = doorIcon.getImage();
         Image newing = image.getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH);
@@ -151,7 +151,7 @@ public class Display extends JPanel {
     }
 
     public void createSouthDoor() {
-        doorIcon = new ImageIcon("src/images/door.png");
+        doorIcon = new ImageIcon("images/door.png");
 
         //scaling image down.
         Image image = doorIcon.getImage();
@@ -173,7 +173,7 @@ public class Display extends JPanel {
     }
 
     public void createEastDoor() {
-        doorIcon = new ImageIcon("src/images/door.png");
+        doorIcon = new ImageIcon("images/door.png");
 
         //scaling image down.
         Image image = doorIcon.getImage();
